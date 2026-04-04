5436d4a898c6501754880010773f10e053d2715f
Drilon Mahmudi <drilonmahmudi@gmail.com>
2026-04-04 13:34:43 +0200

    sdsd

---
+++ screens/ExerciseScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet, View } from 'react-native';
+
+const ExerciseScreen = () => {
+    let message = "Hello World!";ds
+
+    message = "Test";
+    
+    return(
+        <View>
+            <Text>{message}</Text>
+            <Text>{"Hello"}</Text>
+        </View>
+        
+    )
+}
+
+export default ExerciseScreen;
