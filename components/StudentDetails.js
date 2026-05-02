3e6f1112db97f3f41ebaeba1e74d52ecdd9ae266
Drilon Mahmudi <drilonmahmudi@gmail.com>
2026-05-02 10:37:33 +0200

    dd

--- components/StudentDetails.js
+++ components/StudentDetails.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     },
     
     img: {
-        widthass: 100,
+        width: 100,
         height: 100,
         borderTopLeftRadius: 8,
         borderBottomLeftRadius: 8
